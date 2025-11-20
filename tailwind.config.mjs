e6aa3e8c4299adc28bf50208776ba88326ca7514
jemmdevs <jemmdev@gmail.com>
2025-11-20 13:29:09 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#111111',
+        muted: '#71717a',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        base: ['15px', '21px'],
+        label: ['12px', '18px'],
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+      },
+      gridTemplateColumns: {
+        '12': 'repeat(12, minmax(0, 1fr))',
+      },
+      gap: {
+        '3': '12px',
+      },
+    },
+  },
+  plugins: [],
+}
+
